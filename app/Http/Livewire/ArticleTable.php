628d9555968f766840e6290e1a08cb8320712989
SammyJoseph <sam.tab.paz@gmail.com>
2023-09-06 17:46:40 -0500

    clase #9 finalizada

--- app/Http/Livewire/ArticleTable.php
+++ app/Http/Livewire/ArticleTable.php
@@ -5,10 +5,11 @@ namespace App\Http\Livewire;
 use Rappasoft\LaravelLivewireTables\DataTableComponent;
 use Rappasoft\LaravelLivewireTables\Views\Column;
 use App\Models\Article;
+use Illuminate\Database\Eloquent\Builder;
 
 class ArticleTable extends DataTableComponent
 {
-    protected $model = Article::class;
+    // protected $model = Article::class; // se utilizará el método builder() en lugar del modelo
 
     public function configure(): void
     {
@@ -25,5 +26,11 @@ class ArticleTable extends DataTableComponent
             Column::make("Updated at", "updated_at")
                 ->sortable(),
         ];
+    }
+
+    public function builder(): Builder
+    {
+        return Article::query()
+                    ->with('user');
     }
 }
