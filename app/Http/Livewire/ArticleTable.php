2d483848232abb0ed07cb17954f082390dcfed94
SammyJoseph <sam.tab.paz@gmail.com>
2023-09-07 11:54:38 -0500

    clase #20 finalizada

--- app/Http/Livewire/ArticleTable.php
+++ app/Http/Livewire/ArticleTable.php
@@ -48,7 +48,7 @@ class ArticleTable extends DataTableComponent
             Column::make("Creado", "created_at")
                 ->format(fn($value) => $value->format('d/m/Y')),
 
-            ButtonGroupColumn::make("Acciones")
+            /* ButtonGroupColumn::make("Acciones")
                 ->buttons([
                     LinkColumn::make("Acciones")
                         ->title(fn() => "Editar")
@@ -62,9 +62,20 @@ class ArticleTable extends DataTableComponent
                         ->attributes(fn() => [
                             'class' => 'btn-red-gradient'
                         ])
-                ]),
-
+                ]), */
             
+            /* Para poder utilizar el botón Eliminar, se requiere agregar HTML ↑↓ */
+            /* Column::make("Acciones", 'id')
+                ->format(fn($value) => "<a href='/dashboard?id={$value}' class='btn-red-gradient'>
+                                <svg class='inline w-5' fill='currentColor' xmlns='http://www.w3.org/2000/svg' viewBox='0 -960 960 960'><path d='m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z'/></svg>
+                            </a>")
+                ->html(), */
+
+            /* También se puede retornar una vista ↑↓ */
+            Column::make("Acciones", 'id')
+                ->format(fn($value) => view('articles.tables.action', [
+                    'id' => $value
+                ])),
         ];
     }
 
---
+++ resources/views/articles/tables/action.blade.php
@@ -0,0 +1,11 @@
+<a href="{{ route('dashboard', ['id' => $id]) }}" class="btn-blue-gradient">
+    <svg class='inline w-5' fill='currentColor' xmlns='http://www.w3.org/2000/svg' viewBox='0 -960 960 960'>
+        <path d="M200-200h56l345-345-56-56-345 345v56Zm572-403L602-771l56-56q23-23 56.5-23t56.5 23l56 56q23 23 24 55.5T829-660l-57 57Zm-58 59L290-120H120v-170l424-424 170 170Zm-141-29-28-28 56 56-28-28Z"/>
+    </svg>
+</a>
+
+<a href="{{ route('dashboard', ['id' => $id]) }}" class="btn-red-gradient">
+    <svg class='inline w-5' fill='currentColor' xmlns='http://www.w3.org/2000/svg' viewBox='0 -960 960 960'>
+        <path d='m376-300 104-104 104 104 56-56-104-104 104-104-56-56-104 104-104-104-56 56 104 104-104 104 56 56Zm-96 180q-33 0-56.5-23.5T200-200v-520h-40v-80h200v-40h240v40h200v80h-40v520q0 33-23.5 56.5T680-120H280Zm400-600H280v520h400v-520Zm-400 0v520-520Z'/>
+    </svg>
+</a>
